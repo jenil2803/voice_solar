99a5e89ca8bd38c43a71e39bff6e49ba9b723d84
mayanksali05 <salimayank5@gmail.com>
2026-03-07 08:43:32 +0530

    Update sidebar hover effect

--- cook/lib/widgets/sidebar.dart
+++ cook/lib/widgets/sidebar.dart
@@ -124,7 +124,7 @@ class Sidebar extends StatelessWidget {
   }
 }
 
-class _NavItem extends StatelessWidget {
+class _NavItem extends StatefulWidget {
   final IconData? icon;
   final String? assetIcon;
   final bool isActive;
@@ -140,58 +140,75 @@ class _NavItem extends StatelessWidget {
   }) : assert(icon != null || assetIcon != null);
 
   @override
+  State<_NavItem> createState() => _NavItemState();
+}
+
+class _NavItemState extends State<_NavItem> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Stack(
-        children: [
-          Container(
-            width: 48,
-            height: 48,
-            decoration: BoxDecoration(
-              color: isActive ? Colors.white : Colors.transparent,
-              borderRadius: BorderRadius.circular(12),
-              boxShadow: isActive
-                  ? [
-                      BoxShadow(
-                        color: Colors.black.withValues(alpha: 0.05),
-                        blurRadius: 10,
-                        offset: const Offset(0, 4),
-                      )
-                    ]
-                  : null,
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: Stack(
+          children: [
+            AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              width: 48,
+              height: 48,
+              decoration: BoxDecoration(
+                color: widget.isActive
+                    ? Colors.white
+                    : (_isHovered
+                        ? Colors.grey.withValues(alpha: 0.1)
+                        : Colors.transparent),
+                borderRadius: BorderRadius.circular(12),
+                boxShadow: widget.isActive
+                    ? [
+                        BoxShadow(
+                          color: Colors.black.withValues(alpha: 0.05),
+                          blurRadius: 10,
+                          offset: const Offset(0, 4),
+                        )
+                      ]
+                    : null,
+              ),
+              child: widget.assetIcon != null
+                  ? Image.asset(
+                      widget.assetIcon!,
+                      color: widget.isActive || _isHovered
+                          ? const Color(0xFF0EA5E9)
+                          : const Color(0xFF64748B),
+                      width: 24,
+                      height: 24,
+                    )
+                  : Icon(
+                      widget.icon,
+                      color: widget.isActive || _isHovered
+                          ? const Color(0xFF0EA5E9)
+                          : const Color(0xFF64748B),
+                      size: 24,
+                    ),
             ),
-            child: assetIcon != null
-                ? Image.asset(
-                    assetIcon!,
-                    color: isActive
-                        ? const Color(0xFF0EA5E9)
-                        : const Color(0xFF64748B),
-                    width: 24,
-                    height: 24,
-                  )
-                : Icon(
-                    icon,
-                    color: isActive
-                        ? const Color(0xFF0EA5E9)
-                        : const Color(0xFF64748B),
-                    size: 24,
+            if (widget.hasBadge)
+              Positioned(
+                right: 12,
+                top: 12,
+                child: Container(
+                  width: 8,
+                  height: 8,
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF0EA5E9),
+                    shape: BoxShape.circle,
                   ),
-          ),
-          if (hasBadge)
-            Positioned(
-              right: 12,
-              top: 12,
-              child: Container(
-                width: 8,
-                height: 8,
-                decoration: const BoxDecoration(
-                  color: Color(0xFF0EA5E9),
-                  shape: BoxShape.circle,
                 ),
               ),
-            ),
-        ],
+          ],
+        ),
       ),
     );
   }
